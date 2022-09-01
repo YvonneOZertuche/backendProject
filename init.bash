0ac57872f2ecdde36fb125e3dc8b6fcd04dd9bb3
Shannon Kroll <skroll912@gmail.com>
2022-09-01 13:27:23 -0700

    generated the models using sequelize

---
+++ init.bash
@@ -0,0 +1,3 @@
+sequelize model:generate --name roles --attributes type:string
+# sequelize model:generate --name users --attributes username:string,email:string,password:string,password2:string,roleID:integer,isPublished:boolean
+# sequelize model:generate --name blogs --attributes title:string,content:string,isPublished:boolean

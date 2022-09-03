c68f115f76143fc97675e9196f2dea256815a55b
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-09-03 16:57:18 -0500

    additional code added to app.js

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 const express = require('express')
 const app = express()
+const helmet = require('helmet');
 const passport = require('passport');
 require('./auth/passport-config')(passport)
 const cookieSession = require('cookie-session');
-// let PORT = 3000
 // const { pool } = require('./dbconfig')
-
 // require('dotenv').config
-
 const PORT = process.env.PORT || 3000
+
+app.use(express.static('public'))
+app.use(helmet)
 app.set('view engine', 'ejs')
 
-//scrapers
+//SCRAPERS
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-//passport
+//PASSPORT-MIDDLEWARE
 app.use(passport.initialize())
 app.use(passport.session)
 
-cookieSession
+//COOKIE SESSION
 app.use(cookieSession({
   name: 'session',
-  key: ['abcdeabcdeabcde'],
+  keys: ['abcdeabcdeabcde'],
   maxAge: 14 * 24 * 60 *60 * 1000
 }))
 

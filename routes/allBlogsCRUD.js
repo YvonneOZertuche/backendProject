ff041f6a7774bf252ff6c95457035a4dbe608437
Shannon Kroll <skroll912@gmail.com>
2022-09-04 11:12:15 -0700

    udpated the css for the home page and /blogs

--- routes/allBlogsCRUD.js
+++ routes/allBlogsCRUD.js
@@ -7,53 +7,54 @@ const db = require('../models');
 router.use(express.json())
 router.use(express.urlencoded({extended: true}))
 
-// let feedbackObjs = require("../data/feedbackObjs.json")
+//NOT WORKING - display individual blogs on index page
+// router.get('/index', async (req, res) => {
+//     let {id} = req.params
+//     console.log(id)
+//     let data = await db.blogs.findOne({where: {id:id}});
+    
+//     res.render("index", {data: data})
+// })
 
-
+//new blog entry route
 router.get('/newBlogEntry', (req, res) => {
     res.render(`newBlogEntry`, {
-        // "feedbackObjs":feedbackObjs
+       
     })
 })
 
 
-//submit a new message
+//submit a new blog entry
 router.post('/newBlogEntry', async (req, res) => {
     console.log('submit button used')
     //get data from the feedback.ejs header
     let {blogTitle, blogEntry} = req.body 
     console.log(`${blogTitle}, ${blogEntry}`)
 
-    //done: create a db and create a sequelize model in the db
-    //define sequelize models in the js server (that means in the models file but really just here for now so we can make sure it works first)
-    //use model.create to create a new row in the db table (see: https://dc-houston.herokuapp.com/p2/Postgres/Sequelize.html#21)
-
     //I'll need to add another variable when Yvonne is ready to reference the userID via cookies
     console.log(db.blogs)
     await db.blogs.create({title:blogTitle, content:blogEntry})
     
-    
     res.redirect('/newBlogEntry');
 })
 
-//get route - to get all the blog posts
+//get all the blog posts
 router.get('/getBlogs', async (req, res) => {
     try{
         console.log(db.blogs)
-        let data = await db.blogs.findAll();
-        res.render("blogs", {data:data})
+        let data = await db.blogs.findOne({where: {id:id}});
+        res.render("index", {data:data})
     }
     catch(error){
         console.log(error)
     }
 })
 
-//blog edit route
+//blog edit routes
 router.get('/blogEdit/:id', async (req, res) => {
     let {id} = req.params
     // console.log(id)
     let data = await db.blogs.findOne({where: {id:id}});
-    // res.json(data);
     res.render("blogEdit", {data: data})
 })
 
